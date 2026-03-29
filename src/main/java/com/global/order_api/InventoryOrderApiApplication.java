3de5d1c71bfc376d6be09a032537709bc2958aeb
Ahmed Ehab <ahmedehab56200@gmail.com>
2026-03-29 17:40:41 +0200

    intial project setup

---
+++ src/main/java/com/global/order_api/InventoryOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.global.order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/global/order_api/InventoryOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
