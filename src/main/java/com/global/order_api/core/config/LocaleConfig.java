5e3c514378cc342976b8dd5da0f2a4dbf74c2f0c
Ahmed Ehab <ahmedehab56200@gmail.com>
2026-04-01 22:10:45 +0200

    add i18n and messages files

---
+++ src/main/java/com/global/order_api/core/config/LocaleConfig.java
@@ -0,0 +1,30 @@
+package com.global.order_api.core.config;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@Configuration
+public class LocaleConfig {
+	@Bean
+	public LocaleResolver localeResolver()
+	{
+		// class to see headers in api and find Accept-language
+		AcceptHeaderLocaleResolver resolver=new AcceptHeaderLocaleResolver();
+		
+		// set default english if front-end doesn't send header
+		resolver.setDefaultLocale(Locale.ENGLISH);
+		
+		// set languages which our system support
+		resolver.setSupportedLocales(Arrays.asList(
+				new Locale("en"),
+				new Locale("ar")
+				));
+		
+		return resolver;
+	}
+}
