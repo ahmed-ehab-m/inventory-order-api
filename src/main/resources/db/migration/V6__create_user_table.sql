a6b4824cb8abf5936835ee6d41f50e1ce03c564e
Ahmed Ehab <ahmedehab56200@gmail.com>
2026-04-19 21:15:52 +0200

    create user table sql file

---
+++ src/main/resources/db/migration/V6__create_user_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+	
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	location TEXT,
+	phone VARCHAR(50),
+	--not ENUM because if any update to add a new role will be complix solution
+	role VARCHAR(50) NOT NULL ,
+	is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+	
+		    -- from BaseEntity
+    -- CURRENT_TIMESTAMP => db store the date by herself not await to spring to send it
+    created_at  DEFAULT CURRENT_TIMESTAMP, 
+    -- ON UPDATE => any one update this record , update the time automatically
+    updated_at  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by  VARCHAR(255),
+    updated_by  VARCHAR(255)
+	
+)
