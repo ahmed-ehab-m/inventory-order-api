7e42c7d45cc797980d6e17b565d7950ea3c047ea
Ahmed Ehab <ahmedehab56200@gmail.com>
2026-04-28 00:10:17 +0200

    create cart items table

---
+++ src/main/resources/db/migration/V8__create_cart_item_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE cart_items(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    cart_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+
+    -- from BaseEntity
+    -- CURRENT_TIMESTAMP => db store the date by herself not await to spring to send it
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    -- ON UPDATE => any one update this record , update the time automatically
+    updated_at  DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by  VARCHAR(255),
+    updated_by  VARCHAR(255),
+
+    constraint fk_cart_items_cart FOREIGN KEY (cart_id) REFERENCES cart(id),
+    constraint fk_cart_items_product FOREIGN KEY (product_id) REFERENCES products(id)
+)
