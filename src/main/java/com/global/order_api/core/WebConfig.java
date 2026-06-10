bfa59ee6f7945ff4a82484407014edb3f5d35045
Ahmed Ehab <ahmedehab56200@gmail.com>
2026-06-11 00:00:34 +0200

    add web config class

---
+++ src/main/java/com/global/order_api/core/WebConfig.java
@@ -0,0 +1,23 @@
+package com.global.order_api.core;
+
+import com.global.order_api.core.rate_limiting.RateLimitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/// web mvc configurer => interface allows us to configure web settings without editing default
+/// use it to override addInterceptor() to add our custom interceptor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+        /// add our interceptor and specify paths
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/**");
+    }
+}
