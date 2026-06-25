cf74ac537952fba820c93ffcb81cbd2688b0bb3c
Ahmed Ehab <ahmedehab56200@gmail.com>
2026-06-25 06:02:44 +0200

    add test endpoint for displaying user ip address

--- src/main/java/com/global/order_api/feature/user/controller/UserController.java
+++ src/main/java/com/global/order_api/feature/user/controller/UserController.java
@@ -10,6 +10,7 @@ import com.global.order_api.feature.user.service.UserService;
 import com.global.order_api.feature.user.dto.ChangePasswordRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -74,5 +75,18 @@ public class UserController {
         userService.softDeleteUser(userId);
         String message = appTranslator.getTranslatedAction("success.deleted", ENTITY_KEY);
         return ResponseEntity.ok(ApiResponse.success(null, message));
+    }
+
+    /// test ip address
+    @GetMapping("/test-ip")
+    public String testIp(HttpServletRequest request) {
+
+        return """
+            remoteAddr = %s
+            x-forwarded-for = %s
+            """.formatted(
+                request.getRemoteAddr(),
+                request.getHeader("X-Forwarded-For")
+        );
     }
 }
